1be4579b620e7c1fd6db762aceb50712a40f21f4
Shane <shane.r.rookey@gmail.com>
2023-08-24 11:48:08 -0400

    fix responsiveness on fun page

--- app/fun/page.js
+++ app/fun/page.js
@@ -3,13 +3,11 @@ import { InstagramEmbed, YouTubeEmbed } from "react-social-media-embed";
 
 export default function Fun() {
     return (
-        <div className="flex-col max-w-max justify-center mx-auto">
-            <div className="flex">
-                <InstagramEmbed className="m-20" url="https://www.instagram.com/reel/CwLRuT0o5Js/" width={535}/>
-                <InstagramEmbed className="m-20" url="https://www.instagram.com/reel/ChSMRkhFWeA" width={535}/>
-            </div>
-            <YouTubeEmbed className="mx-auto" url="https://youtube.com/watch?v=WBnqKxVd5E8" width={600} height={800}/>
-            <h1 className="text-center text-teal-700 text-4xl mx-40 mt-20 mb-40 font-bold dark:text-white">New Projects coming soon...</h1>
+        <div className="flex-1 max-w-max justify-center mx-auto">
+            <InstagramEmbed url="https://www.instagram.com/reel/CwLRuT0o5Js/" />
+            <InstagramEmbed url="https://www.instagram.com/reel/ChSMRkhFWeA" />
+            <YouTubeEmbed url="https://youtube.com/watch?v=WBnqKxVd5E8" width="100%" height="600" />
+            <h1 className="text-center text-teal-700 my-10 text-4xl font-bold dark:text-white">New Projects coming soon...</h1>
         </div>
     );
 }
