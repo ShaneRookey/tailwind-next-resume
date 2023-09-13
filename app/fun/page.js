5327b6c03d03f9e4c63da6264231c421eeabeb51
Shane <shane.r.rookey@gmail.com>
2023-09-13 11:02:47 -0400

    update dropdown and suspend the modelviewer

--- app/fun/page.js
+++ app/fun/page.js
@@ -1,25 +1,31 @@
 'use client'
-import ModelViewer from "@/components/3d/modelviewer";
+import { CircularProgress, Grid } from "@mui/material";
 import Image from "next/image";
+import React, { Suspense } from "react";
 import { InstagramEmbed, YouTubeEmbed } from "react-social-media-embed";
-import dg from "../../public/dg.png";
+
+const ModelViewer = React.lazy(() => import("@/components/3d/modelviewer"))
 
 export default function Fun() {
     return (
-        <>
-            <div className="mx-auto lg:w-1/2 h-screen mb-20">
-                <h1 className="text-center text-teal-700 text-4xl font-bold dark:text-white">Interactive Model</h1>
-                <ModelViewer />
-            </div>
-            <div className="mb-5">
-                <Image priority src={dg} alt="Disc Golf Render" className="mx-auto" quality={100}/>
-            </div>
-            <div className="sm:flex-1 lg:flex-row max-w-max justify-center mx-auto mb-5">
+        <Grid container columns={{xs:1, md: 3}}>
+            <Grid item md={1} className="p-5 flex justify-center align-middle">
+                <Suspense fallback={<CircularProgress size={100}/>}>
+                    <ModelViewer />
+                </Suspense>
+            </Grid>
+            <Grid item md={2} className="p-5">
+                <Image priority src="/dg.png" alt="Disc Golf Render"  quality={100} width={1920} height={1080}/>
+            </Grid>
+            <Grid item md={1} className="p-5">
                 <InstagramEmbed url="https://www.instagram.com/reel/CwLRuT0o5Js/" />
+            </Grid>
+            <Grid item md={1} className="p-5">
+                <YouTubeEmbed url="https://youtube.com/watch?v=WBnqKxVd5E8" width="100%"/>
+            </Grid>
+            <Grid item md={1} className="p-5">
                 <InstagramEmbed url="https://www.instagram.com/reel/ChSMRkhFWeA/" />
-                <YouTubeEmbed url="https://youtube.com/watch?v=WBnqKxVd5E8" width="100%" height="600" />
-                <h1 className="text-center text-teal-700 my-10 text-4xl font-bold dark:text-white">New Projects coming soon...</h1>
-            </div>
-        </>
+            </Grid>
+        </Grid>
     );
 }
